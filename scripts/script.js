6b4f27cf91337f1796b72339850c6f911af1c44f
Geoffrey <geoffrpayton@gmail.com>
2021-04-29 22:14:13 -0400

    include possible API endpoint for currency conversion

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // https://www.cheapshark.com/api/1.0
 // https://www.cheapshark.com/api/1.0/deals
 // https://www.cheapshark.com/api/1.0/games
+// https://api.ratesapi.io/api/latest?base=USD
 
 // Cache selectors
 
