b4a134cd083e06996ba95b06d629c6d322ff32b3
Geoffrey <geoffrpayton@gmail.com>
2021-04-29 22:05:17 -0400

    create initial files, initialize scss and setup snippet, confirm script.js is connected

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// https://www.cheapshark.com/api/1.0
+// https://www.cheapshark.com/api/1.0/deals
+// https://www.cheapshark.com/api/1.0/games
+
+// Cache selectors
+
+// Create namespace object
+
+// Add event listeners
+
+// Function to call API
+
+// Function to render results to the DOM
+
+// Helper function to convert currency
