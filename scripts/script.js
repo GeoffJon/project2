6ea7734428d3ae205e0cce0c2c0efe7da46fb34d
Geoffrey <geoffrpayton@gmail.com>
2021-05-01 15:19:50 -0400

    added filter function to combine prices into one object

--- scripts/script.js
+++ scripts/script.js
@@ -11,13 +11,9 @@ const searchTitle = document.getElementById('searchTitle');
 const baseURL = new URL('https://www.cheapshark.com/api/1.0/deals');
 baseURL.search = new URLSearchParams({
     storeID: '1, 7',
-    title: searchTitle.value
+    title: searchTitle.value,
+    sortBy: 'Title'
   });
-  
-  // const baseURL = new URL('https://www.cheapshark.com/api/1.0/deals');
-  // const searchParams = new URLSearchParams(baseURL);
-
-  // searchParams.set('storeID', '1');
 
 
 // Create namespace object
@@ -35,9 +31,8 @@ app.init = () => {
 // Function to call API to get random games
 app.getRandomGames = (title) => {
   console.log(title);
-  // baseURL.search.set('title', title);
-  // searchParams.append('title', title)
-  baseURL.searchParams.set('title', searchTitle.value)  
+  baseURL.searchParams.set('title', searchTitle.value);
+  // baseURL.searchParams.set('onSale', 1);
   fetch(baseURL)
     .then(response => response.json())
     .then(data => {
@@ -50,11 +45,54 @@ app.getRandomGames = (title) => {
       console.log(list);
       gamesList.innerHTML = '';
 
-      list.forEach(deal => {
+      let finalGames = [];
+      
+      const getGamePrices = (array) => {
+        // Initialize current empty game object
+        let currentGame = {};
+        // Bring in first game in array
+        array.forEach((game, index) => {
+          const { title, normalPrice, salePrice, savings } = game;
+          // Initialize currentGame with first object so empty object does not get pushed
+          if (!index) {
+            currentGame = { ...game }
+            console.log(currentGame);
+          } else
+            // If game title matches current game title, pull out price and set to object
+            if (currentGame.title === title) {
+              currentGame.price2 = salePrice;
+              console.log(currentGame);
+            } else
+              // If game title does not match current game title, push object to final array and reset current object
+              if (currentGame.title !== title) {
+                finalGames.push(currentGame);
+                console.log({ finalGames });
+                currentGame = { ...game };
+              }
+        });
+        // Push final currentGame object at end of loop
+        finalGames.push(currentGame);
+        console.log(finalGames);
+      }
+
+      getGamePrices(list);
+      
+      finalGames.forEach(deal => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${deal.title}`;
+        const {
+          title,
+          normalPrice,
+          salePrice,
+          savings,
+          price2
+        } = deal;
+        listItem.textContent = `${title} --> MSRP: $${normalPrice}, Sale: $${salePrice}, ${price2 === undefined || price2 === `` ? `` : `Sale2: $${price2}`} (-${Math.trunc(savings)}%)`;
         gamesList.appendChild(listItem);
       });
+      
+      
+      
+      
     }
   
     // setTimeout(getData(returnedList), 2000);
@@ -65,6 +103,9 @@ app.getRandomGames = (title) => {
 // Function to render results to the DOM
 
 // Helper function to convert currency
+
+
+
 
 
 app.init();
