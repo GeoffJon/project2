d5915ec0156dcec4ebfbb2f321cbd1c0062476ad
Geoffrey <geoffrpayton@gmail.com>
2021-05-04 00:19:40 -0400

    new method using ::before element

--- scripts/script.js
+++ scripts/script.js
@@ -174,8 +174,8 @@ app.updateData = (gamesArray) => {
       <td><div class="gameCover"><img src="${deal.thumb}"></div></td>
       <td>${title}</td>
       <td>$${normalPrice}</td>
-      <td class="storeLink"><a href="https://www.cheapshark.com/redirect?dealID=${steamID}" class="${app.getDiscount(steamSavings)}" target="_blank">$${steamPrice || `--`}</a><span class="discountPercentage">${Number(savings).toFixed(0)}% off</span></td>
-      <td><a href="https://www.cheapshark.com/redirect?dealID=${gogID}" savings="${savings}% off" class="storeLink ${app.getDiscount(gogSavings)}" target="_blank">$${gogPrice || `--`}</a></td>
+      <td><a href="https://www.cheapshark.com/redirect?dealID=${steamID}" savings="-${Number(savings).toFixed(0)}%" class="storeLink ${app.getDiscount(steamSavings)}" target="_blank">$${steamPrice || `--`}</a></td>
+      <td><a href="https://www.cheapshark.com/redirect?dealID=${gogID}" savings="-${Number(savings).toFixed(0)}%" class="storeLink ${app.getDiscount(gogSavings)}" target="_blank">$${gogPrice || `--`}</a></td>
       `
 
     gamesList.appendChild(tableRow);
